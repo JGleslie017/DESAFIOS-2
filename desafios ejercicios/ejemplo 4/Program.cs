6c9e19367383a1efc01bb4d4ad0462df2d5c5dc4
JGleslie017 <126440192+JGleslie017@users.noreply.github.com>
2023-03-17 20:27:24 -0600

    Add files via upload
    0.1

---
+++ desafios ejercicios/ejemplo 4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejemplo_4
+{
+    internal class Superficiefrontal
+    {
+        static void Main(string[] args)
+
+        {
+            double Base, Altura, Area;
+            Console.Write("ingrese la Base:");
+            Base = double.Parse(Console.ReadLine());
+                 Console.Write("ingrese la Altura:");
+            Altura = double.Parse(Console.ReadLine());
+                Area = Base * Altura
+                Console.writeline("el area es:{0}", Area );
+            Console.ReadLine();
+
+        }
+
+    }
+}
